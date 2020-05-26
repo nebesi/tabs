ae3e9e4a7d31b748adfa7e4d95fbecdf7accf303
max <mirseryozha@yandex.ru>
2020-05-26 22:45:08 +0300

    secondTab

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function() {
+
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelectorAll('.info-header'),
+        info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++){
+        for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
@@ -14,22 +15,23 @@ window.addEventListener('DOMContentLoaded', function(){
     hideTabContent(1);
 
     function showTabContent(b) {
-        if(tabContent[b].classList.contains('hide')) {
+        if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
-
     }
-    info.addEventListener('click', function(event){
+
+    info.addEventListener('click', function(event) {
         let target = event.target;
-        if(target && target.classList.contains('info-header-tab')) {
-            for(let i = 0; i <tab.length; i++) {
-                if(target == tab[i]) {
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
                     break;
                 }
             }
         }
+
     });
 });
